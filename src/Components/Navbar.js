5e0fd642d2d3a0d951844c276307d1c80d1c6a3e
Shreya M <shreyam.dsce123@gmail.com>
2025-04-08 00:47:48 +0530

    changes in the ui of the website

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,14 +5,14 @@ function Navbar() {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid justify-content-center">
+        <div className="container-fluid">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse " id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2  mb-lg-0">
+          <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+            <ul className="navbar-nav">
               <li className="nav-item px-5">
-                <a className="nav-link " aria-current="page" href="#home">Home</a>
+                <a className="nav-link" aria-current="page" href="#home">Home</a>
               </li>
               <li className="nav-item px-5">
                 <a className="nav-link" href="#about">About</a>
